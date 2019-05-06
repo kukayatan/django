2ee48af2bc57153115a132568b91ca6bbfca645f
Jan Kohout <admin@admin.cz>
2019-05-06 09:03:01 +0200

    form adjustement

--- wordcounter/forms.py
+++ wordcounter/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class WordForm(forms.Form):
-    wform = forms.CharField(label='',widget=forms.Textarea())
+    wform = forms.CharField(label='',widget=forms.Textarea(attrs={"rows":15,"cols":70}))
